c75ef7da8593e26bb892841b94837abf6c0ce9ff
Alessandra Souza <lupina.sp@gmail.com>
2020-01-05 22:46:31 -0300

    routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,38 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { PostsComponent } from './posts/posts.component';
+import { PostDetailComponent } from './post-detail/post-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', 
+    component: PostsComponent,
+    data: {
+      title: 'Postagem',
+      animation: 'DetailPage'
+    }
+  },
+  {
+    path: 'post/:id',
+    component: PostDetailComponent,
+    data: {
+      title: 'Detalhes da Postagem',
+      animation: 'PostsPage'
+    }
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes)
+  ],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+  
+}
