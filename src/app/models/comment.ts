aa579bdca191810b2e79694d30a4f1ea37e5c994
Alessandra Souza <lupina.sp@gmail.com>
2020-01-05 22:48:03 -0300

    models: user, post, comments

---
+++ src/app/models/comment.ts
@@ -0,0 +1,7 @@
+export class Comment {
+    postId: Number;
+    id: Number;
+    name: String;
+    body: String;
+    email: String;
+}
---
+++ src/app/models/user.ts
@@ -0,0 +1,7 @@
+export class User {
+    id: Number;
+    name: String;
+    username: String;
+    email: String;
+
+}
