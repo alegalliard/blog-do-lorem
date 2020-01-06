e620e79cf411d057270e10a3e660e13ae510f1c9
Alessandra Souza <lupina.sp@gmail.com>
2020-01-05 22:50:17 -0300

    user component

---
+++ src/app/user/user-mock.ts
@@ -0,0 +1,9 @@
+import { User } from './user';
+
+export const USERS: User[] = [
+    { id: 1, name: 'Monica', avatar: 'toy-monica.png' },
+    { id: 2, name: 'Magali', avatar: 'toy-magali.png' },
+    { id: 3, name: 'Cascao', avatar: 'toy-cascao.png' },
+    { id: 4, name: 'Cebolinha', avatar: 'toy-cebolinha.png' },
+    { id: 5, name: 'Bidu', avatar: 'toy-bidu.png' }
+];
---
+++ src/app/user/user.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { User } from '../models/user';
+ 
+//DESCARTAR ESSE ARQHUVO
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private userUrl = 'https://jsonplaceholder.typicode.com/users';
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(this.userUrl);
+
+  }
+
+  getUser(id: number): Observable<User> {
+    const url = `${this.userUrl}/${id}`;
+
+    return this.http.get<User>(url);
+  }
+}
